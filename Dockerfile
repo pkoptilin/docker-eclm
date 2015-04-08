bdd57905ec2bac604ba027564a0f265e393a68ef
pkoptilin <pkoptilin@selectica.com>
2015-04-08 16:48:23 +0300

    fix init

--- Dockerfile
+++ Dockerfile
@@ -5,6 +5,7 @@ COPY ojdbc6-11.2.0.3.jar /opt/jboss/wildfly/modules/oracle/jdbc/main/ojdbc6-11.2
 
 COPY init.sh /opt/jboss/init.sh
 COPY datasource.xml /opt/jboss/datasource.xml
+COPY cache-container.xml /opt/jboss/cache-container.xml
 
 RUN /opt/jboss/wildfly/bin/add-user.sh admin Admin#70365 --silent
 #RUN sed -i '/<datasources>/r /opt/jboss/datasource.xml' /opt/jboss/wildfly/standalone/configuration/standalone-full-ha.xml
@@ -13,7 +14,6 @@ RUN /opt/jboss/wildfly/bin/add-user.sh admin Admin#70365 --silent
 
 EXPOSE 9990
 
-#CMD ["/opt/jboss/wildfly/bin/standalone.sh", "--server-config", "standalone-full-ha.xml", "-b", "0.0.0.0", "-bmanagement", "0.0.0.0"]
 
 CMD /opt/jboss/init.sh; \
     /opt/jboss/wildfly/bin/standalone.sh --server-config standalone-full-ha.xml -b 0.0.0.0 -bmanagement 0.0.0.0
--- init.sh
+++ init.sh
@@ -6,8 +6,8 @@ sed -i.bak "s/<connection-url>.*<\/connection-url>/<connection-url>$JDBCURL<\/co
 sed -i.bak '/<datasources>/r /opt/jboss/datasource.xml' /opt/jboss/wildfly/standalone/configuration/standalone-full-ha.xml
 
 sed -i.bak "s/default-stack=\"udp\"/default-stack=\"tcp\"/" /opt/jboss/wildfly/standalone/configuration/standalone-full-ha.xml
-#sed "s/<protocol type=\"MPING\" socket-binding=\"jgroups-mping\"\/>/<protocol type=\"TCPPING\"><property name=\"initial_hosts\">localhost\[7600\]<\/property><property name=\"port_range\">0<\/property><\/protocol>/" /opt/jboss/wildfly/standalone/configuration/standalone-full-ha.xml
+sed -i.bak "s/<protocol type=\"MPING\" socket-binding=\"jgroups-mping\"\/>/<protocol type=\"TCPPING\"><property name=\"initial_hosts\">localhost\[7600\]<\/property><property name=\"port_range\">0<\/property><\/protocol>/" /opt/jboss/wildfly/standalone/configuration/standalone-full-ha.xml
 
 sed -i.bak 's/<cache-container name=\"web\"/<!--<cache-container name=\"web\"/' /opt/jboss/wildfly/standalone/configuration/standalone-full-ha.xml
 sed -i.bak 's/<cache-container name=\"ejb\"/--> <cache-container name=\"ejb\"/' /opt/jboss/wildfly/standalone/configuration/standalone-full-ha.xml
-sed -i.bak '/subsystem xmlns="urn:jboss:domain:infinispan:2.0"/r cache-container.xml' /opt/jboss/wildfly/standalone/configuration/standalone-full-ha.xml
+sed -i.bak '/subsystem xmlns=\"urn:jboss:domain:infinispan:2.0\"/r /opt/jboss/cache-container.xml' /opt/jboss/wildfly/standalone/configuration/standalone-full-ha.xml
